3a79196ce1c293392abe6b5784ee4b60d63f39c8
Shyamal <shyamalpatel136@gmail.com>
2025-01-22 15:25:30 -0500

    i just changed the numbers

--- will_return_sum_of_three_numbers_correctly.sh
+++ will_return_sum_of_three_numbers_correctly.sh
@@ -2,9 +2,9 @@
 
 # file: will_return_sum_of_two_numbers_correctly.sh
 
-RESULT=$(./sum_up_to_three_numbers.sh 13 15 11)
+RESULT=$(./sum_up_to_three_numbers.sh 23 5 51)
  
-if [[ $RESULT -eq 39 ]];
+if [[ $RESULT -eq 79 ]];
 then
 	echo "Success"
 else 
